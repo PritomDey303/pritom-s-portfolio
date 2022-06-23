23823c21b60dea87e61bd3871be576ffe2764dd6
Pritom Dey <pritomdey4@gmail.com>
2022-06-23 14:17:22 +0600

    sss

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const authRouter = require("./router/authRouter");
 //dotenv file config
 dotenv.config();
 //console.log(dotenv.config());
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 //database connection
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING)
@@ -42,6 +42,6 @@ app.use(notFoundHandler);
 
 // common error handler
 app.use(errorHandler);
-app.listen(port || 5000, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
